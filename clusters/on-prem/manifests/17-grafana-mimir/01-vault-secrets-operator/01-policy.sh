227e56550b2d0ace00ba5caf2e0c3fc3a5ccadc4
daniarmas <daniel.armas9706@gmail.com>
2025-10-12 20:21:08 -0400

    Fixes

--- clusters/on-prem/manifests/17-grafana-mimir/01-vault-secrets-operator/01-policy.sh
+++ clusters/on-prem/manifests/17-grafana-mimir/01-vault-secrets-operator/01-policy.sh
@@ -3,14 +3,14 @@ set -e
 
 export VAULT_ADDR=http://127.0.0.1:8200
 
-vault policy write mimir - <<EOF
-# Allow reading mimir secrets
-path "secret/data/mimir" {
+vault policy write grafana-mimir-configuration - <<EOF
+# Allow reading grafana-mimir secrets
+path "secret/data/grafana-mimir/app/configuration" {
   capabilities = ["read"]
 }
 
-path "secret/metadata/mimir" {
-  capabilities = ["read", "list"]
+path "secret/metadata/grafana-mimir/app/configuration" {
+  capabilities = ["read"]
 }
 
 # Allow token operations
