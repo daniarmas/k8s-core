6d3ddc34aa1550e779c50fbd1d5f719298fbeb7d
Daniel Armas <daniel.armas9706@gmail.com>
2026-01-18 19:32:55 -0500

    fix_cnpg

--- clusters/on-prem/manifests/13-cnpg/01-vault-secrets-operator/01-postgres/02-role.sh
+++ clusters/on-prem/manifests/13-cnpg/01-vault-secrets-operator/01-postgres/02-role.sh
@@ -3,8 +3,8 @@ set -e
 
 export VAULT_ADDR=http://127.0.0.1:8200
 
-vault write auth/kubernetes/role/harbor-postgres \
-    bound_service_account_names=harbor \
-    bound_service_account_namespaces=harbor \
-    policies=harbor-postgres \
+vault write auth/kubernetes/role/core-api-postgres \
+    bound_service_account_names=postgres \
+    bound_service_account_namespaces=core-api \
+    policies=core-api-postgres \
     ttl=24h
